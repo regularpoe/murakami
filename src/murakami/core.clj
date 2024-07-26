a12a41383a6388288e1dee8dcf44bb496cadf51d
regularpoe <vedranvinko@protonmail.com>
2024-07-26 21:54:36 +0200

    Add simple test template and try to inject converted HTML

--- src/murakami/core.clj
+++ src/murakami/core.clj
@@ -1,8 +1,24 @@
 (ns murakami.core
   (:gen-class)
   (:require [markdown.core :as markdown]
+            [selmer.parser :as selmer]
             [clojure.java.io :as io]
             [clojure.string :as str]))
+
+(def template "
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Markdown to HTML</title>
+</head>
+<body>
+    <h1>Markdown Content</h1>
+    <div>
+        {{content | safe}}
+    </div>
+</body>
+</html>  
+")
 
 (defn is-markdown-file? [file]
   (str/ends-with? (.getName (io/file file)) ".md"))
@@ -10,11 +26,13 @@
 (defn read-markdown-files [dir]
   (let [files (filter is-markdown-file? (file-seq (io/file dir)))]
     (doseq [file files]
-      (println "Printing HTML contents of:" (.getName file))
+      (println "Rendering HTML content of:" (.getName file))
       (println "----------------------------------------")
       (let [content (slurp file)
-            html-content (markdown/md-to-html-string content)]
-        (println html-content)))))
+            html-content (markdown/md-to-html-string content)
+            rendered-html (selmer/render template {:content html-content})]
+        (println rendered-html))
+      (println "----------------------------------------"))))
 
 (defn -main [& args]
   (if-let [dir (first args)]
