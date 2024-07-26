ee4887b22d9917906f1fa667ac67d81674387424
regularpoe <vedranvinko@protonmail.com>
2024-07-26 20:53:45 +0200

    Add is-markdown-file? and read-markdown-files

--- src/murakami/core.clj
+++ src/murakami/core.clj
@@ -1,5 +1,15 @@
 (ns murakami.core
-  (:gen-class))
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]))
+
+(defn is-markdown-file? [file]
+  (str/ends-with? (.getName (io/file file)) ".md"))
+
+(defn read-markdown-files [dir]
+  (let [files (filter is-markdown-file? (file-seq (io/file dir)))]
+    (doseq [file files]
+      (println "Printing content of:" (.getName file))
+      (println (slurp file)))))
 
 (defn -main
   "I don't do a whole lot ... yet."
