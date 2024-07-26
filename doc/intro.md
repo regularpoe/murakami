1c004a39fcfb5f2bd04a5303087826cc11e9be14
regularpoe <vedranvinko@protonmail.com>
2024-07-26 20:24:14 +0200

    init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to murakami
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/murakami/core_test.clj
@@ -0,0 +1,7 @@
+(ns murakami.core-test
+  (:require [clojure.test :refer :all]
+            [murakami.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
