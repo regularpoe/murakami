663e592561a8dd8569977ed829bbe0f691e836f1
regularpoe <vedranvinko@protonmail.com>
2024-07-26 21:45:57 +0200

    Add md-to-html-string

--- src/murakami/core.clj
+++ src/murakami/core.clj
@@ -1,6 +1,7 @@
 (ns murakami.core
   (:gen-class)
-  (:require [clojure.java.io :as io]
+  (:require [markdown.core :as markdown]
+            [clojure.java.io :as io]
             [clojure.string :as str]))
 
 (defn is-markdown-file? [file]
@@ -9,8 +10,11 @@
 (defn read-markdown-files [dir]
   (let [files (filter is-markdown-file? (file-seq (io/file dir)))]
     (doseq [file files]
-      (println "Printing content of:" (.getName file))
-      (println (slurp file)))))
+      (println "Printing HTML contents of:" (.getName file))
+      (println "----------------------------------------")
+      (let [content (slurp file)
+            html-content (markdown/md-to-html-string content)]
+        (println html-content)))))
 
 (defn -main [& args]
   (if-let [dir (first args)]
