7898f3babdad69ef6f70dc494a40a0283b85b3de
regularpoe <vedranvinko@protonmail.com>
2024-07-26 21:49:48 +0200

    Add selmer

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/tools.namespace "1.5.0"]
-                 [markdown-clj "1.10.0"]]
+                 [markdown-clj "1.10.0"]
+                 [selmer "1.12.61"]]
   :main ^:skip-aot murakami.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
