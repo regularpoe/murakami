8a46ff4b709bfa1e130e23f50009783643993033
regularpoe <vedranvinko@protonmail.com>
2024-07-28 08:33:27 +0200

    Bump to 1.0.0 and change description, remove existing LICENSE

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject murakami "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+(defproject murakami "1.0.0"
+  :description "Convert MD to HTML template"
+  :url "https://github.com/regularpoe/murakami"
+  :license {:name "GNU GENERAL PUBLIC LICENSE Version 3"
+            :url ""}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/tools.namespace "1.5.0"]
                  [markdown-clj "1.10.0"]
